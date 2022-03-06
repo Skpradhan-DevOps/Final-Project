e555acc0871d2e1528fddb838f921c118150bbc0
Skpradhan-DevOps <84652480+Skpradhan-DevOps@users.noreply.github.com>
2022-03-06 09:47:02 -0500

    Update TestTitle.java

--- selenium-testng-maven/src/main/java/com/TestTitle.java
+++ selenium-testng-maven/src/main/java/com/TestTitle.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestTitle {
 	//private RemoteWebDriver driver = null;
@@ -23,29 +22,16 @@ public class TestTitle {
 	
 	@Test
 	public void beforeTest() throws InterruptedException, MalformedURLException {
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions option= new ChromeOptions();
-		WebDriver driver = new ChromeDriver(option);
 		
-		URL url=new URL("http://localhost:4444");
-		RemoteWebDriver drivers = new RemoteWebDriver(url,(Capabilities) driver);
-		
-		drivers.get("https://www.aetna.com/");
-		Thread.sleep(4000);
-		String title = drivers.getTitle();
-		System.out.println("Title is : "+title);
-		Assert.assertTrue(title.contains("Health Insurance Plans | Aetna"));
-		
-		
-		/*DesiredCapabilities dc = DesiredCapabilities.chrome();
+		DesiredCapabilities dc = DesiredCapabilities.chrome();
 		URL url=new URL("http://localhost:4444");
 		RemoteWebDriver driver = new RemoteWebDriver(url,dc);
 		driver.get("https://www.aetna.com/");
 		Thread.sleep(4000);
 		String title = driver.getTitle();
-		Assert.assertTrue(title.contains("Health Insurance Plans | Aetna"));*/
+		Assert.assertTrue(title.contains("Health Insurance Plans | Aetna"));
 		
-		drivers.quit();
+		driver.quit();
 	}
 
 	/*@Test
