44f2cd815d844233522cbfd6db76b37a8675151d
Skpradhan-DevOps <84652480+Skpradhan-DevOps@users.noreply.github.com>
2022-03-05 21:20:54 -0500

    Update TestTitle.java

--- selenium-testng-maven/src/main/java/com/TestTitle.java
+++ selenium-testng-maven/src/main/java/com/TestTitle.java
@@ -2,7 +2,7 @@ package com;
 
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
@@ -11,7 +11,7 @@ import org.testng.annotations.Parameters;
 public class TestTitle {
 	private WebDriver driver = null;
 
-	// public String URL = "http://www.nguoianphu.com";
+	// public String URL = "https://www.aetna.com/";
 
 	@Test
 	@Parameters({ "myURL", "myTitle" })
@@ -20,12 +20,12 @@ public class TestTitle {
 		String title = driver.getTitle();
 		Assert.assertTrue(title.contains(myTitle));
 		// Assert.assertTrue(title
-		// .contains("Home page Nguoi An Phu huyen An Phu tinh An Giang"));
+		// .contains("Health Insurance Plans | Aetna"));
 	}
 
 	@BeforeTest
 	public void beforeTest() throws Exception {
-		driver = new FirefoxDriver();
+		driver = new ChromeDriver();
 	}
 
 	@AfterTest
